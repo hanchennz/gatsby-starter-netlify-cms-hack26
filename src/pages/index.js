a0f00ed7cbd4d341ce603567f0968f821ead536d
Han Chen <chenhan22@gmail.com>
2018-08-10 10:11:38 -0700

    Add console.log to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,36 +3,42 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link'
 
 
-
 const randomColors = ["#B89AFE", "#7BCAE1", "FFCE73", "FF8E8E", "8CEFFD"]
 export default class LandingPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    constructor(props) {
+        super(props);
+        // Don't call this.setState() here!
+        console.log("This is a console.log in the LandingPage component's constructor.")
+        console.log("props", props)
+    }
 
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Marketing Pages</h1>
-          </div>
-          {posts
-            .map(({ node: post }, index) => (
-              <div
-                className={'content' + index}
-                key={post.id}
-              >
-                <p>
-                  <Link to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                </p>
-              </div>
-            ))}
-        </div>
-      </section>
-    )
-  }
+    render() {
+        const {data} = this.props;
+        const {edges: posts} = data.allMarkdownRemark;
+
+        return (
+            <section className="section">
+                <div className="container">
+                    <div className="content">
+                        <h1 className="has-text-weight-bold is-size-2">Marketing Pages</h1>
+                    </div>
+                    {posts
+                        .map(({node: post}, index) => (
+                            <div
+                                className={'content' + index}
+                                key={post.id}
+                            >
+                                <p>
+                                    <Link to={post.fields.slug}>
+                                        {post.frontmatter.title}
+                                    </Link>
+                                </p>
+                            </div>
+                        ))}
+                </div>
+            </section>
+        )
+    }
 }
 
 export const pageQuery = graphql`
